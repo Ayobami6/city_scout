ff7aed7a8e26150cbb7ad853330772b3fc4ab502
Ayobami6 <ayobamidele006@gmail.com>
2025-03-22 23:00:38 +0100

    Implement password hashing and comparison functions using bcrypt

--- auth_service/utils.go
+++ auth_service/utils.go
@@ -1,12 +1,24 @@
 package authservice
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func GenerateAPIKey() string {
 	return uuid.New().String()
 
 }
 
-func HashPassword(password string) string {
-	return password
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func ComparePassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
 }
